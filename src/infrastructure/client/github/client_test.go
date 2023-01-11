3f89e8b56f8ae16bc973c1f37bca9a736d4de52a
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-01-11 15:56:05 +0900

    fix: parse from text for svg of contribution

--- src/infrastructure/client/github/client_test.go
+++ src/infrastructure/client/github/client_test.go
@@ -15,18 +15,18 @@ func TestGitHubGet(t *testing.T) {
 
 	reqUrl := fmt.Sprintf("https://github.com/users/%s/contributions", username)
 	httpmock.RegisterResponder("GET", reqUrl,
-		httpmock.NewStringResponder(200, "<rect data-date=\"2006-01-02\" data-count=\"1\" /><rect data-date=\"2006-01-01\" data-count=\"2\" />"))
+		httpmock.NewStringResponder(200, "<rect data-date=\"2006-01-02\">9 contributions on January 2, 2006</rect><rect data-date=\"2006-01-01\">12 contributions on January 1, 2006</rect>"))
 
 	r, err := c.Get(time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC))
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if r.YesterdayCount != 2 {
-		t.Errorf("Failed: Name is not matched, actual: [%d], expected: [%d]", r.YesterdayCount, 2)
+	if r.YesterdayCount != 12 {
+		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.YesterdayCount, 12)
 	}
-	if r.BaseDateCount != 1 {
-		t.Errorf("Failed: Name is not matched, actual: [%d], expected: [%d]", r.BaseDateCount, 1)
+	if r.BaseDateCount != 9 {
+		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.BaseDateCount, 9)
 	}
 	info := httpmock.GetCallCountInfo()
 	if info[fmt.Sprintf("GET %s", reqUrl)] != 1 {
