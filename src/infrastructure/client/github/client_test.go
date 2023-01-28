b58c3ea8510f7619e292382cffa44daad4d78a51
Kazuki_Kamizuru <k.amago0721@gmail.com>
2023-01-28 19:49:44 +0900

    feat: use graphql instead svg when get contribute count by github

--- src/infrastructure/client/github/client_test.go
+++ src/infrastructure/client/github/client_test.go
@@ -1,7 +1,6 @@
 package github
 
 import (
-	"fmt"
 	"testing"
 	"time"
 
@@ -13,23 +12,46 @@ func TestGitHubGet(t *testing.T) {
 	c := githubClient{username: username}
 	httpmock.Activate()
 
-	reqUrl := fmt.Sprintf("https://github.com/users/%s/contributions", username)
-	httpmock.RegisterResponder("GET", reqUrl,
-		httpmock.NewStringResponder(200, "<rect data-date=\"2006-01-02\">1 contribution on January 2, 2006</rect><rect data-date=\"2006-01-01\">12 contributions on January 1, 2006</rect>"))
+	httpmock.RegisterResponder("POST", "https://api.github.com/graphql",
+		httpmock.NewStringResponder(200, `
+    {
+      "data": {
+        "user": {
+          "contributionsCollection": {
+            "contributionCalendar": {
+              "weeks": [
+                {
+                  "contributionDays": [
+                    {
+                      "contributionCount": 9,
+                      "date": "2006-01-01"
+                    }
+                  ]
+                },
+                {
+                  "contributionDays": [
+                    {
+                      "contributionCount": 12,
+                      "date": "2006-01-02"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+      }
+    }`))
 
 	r, err := c.Get(time.Date(2006, 1, 2, 0, 0, 0, 0, time.UTC))
 	if err != nil {
 		t.Error(err)
 		return
 	}
-	if r.YesterdayCount != 12 {
-		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.YesterdayCount, 12)
+	if r.YesterdayCount != 9 {
+		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.YesterdayCount, 9)
 	}
-	if r.BaseDateCount != 1 {
-		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.BaseDateCount, 1)
-	}
-	info := httpmock.GetCallCountInfo()
-	if info[fmt.Sprintf("GET %s", reqUrl)] != 1 {
-		t.Errorf("Failed: GET %s is not called", reqUrl)
+	if r.BaseDateCount != 12 {
+		t.Errorf("Failed: Count is not matched, actual: [%d], expected: [%d]", r.BaseDateCount, 12)
 	}
 }
